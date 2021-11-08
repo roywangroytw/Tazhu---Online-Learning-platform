249c44d7e1f53997befe967bea6ccff2b3ab6a1b
roywangroytw <roywangroy.tw@gmail.com>
2021-11-08 14:46:56 +0800

    setting first route, controller, and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # path 1: "/" 根目錄
+  get "/", to: "pages#index"
+  # 舊式的寫法：get "/", controller: "pages", action: "index"
+
 end
