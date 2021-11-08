7475ae69a68ad447fd51ab44f243d97bc7f23737
roywangroytw <roywangroy.tw@gmail.com>
2021-11-08 15:38:02 +0800

    added about

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
-class PagesController < ActionController::Base
+class PagesController < ApplicationController
     def index
+        # 渲染在 app/views/pages/index.html.erb檔
+    end
+
+    def about
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   # path 1: "/" 根目錄
   get "/", to: "pages#index"
   # 舊式的寫法：get "/", controller: "pages", action: "index"
+  # root "pages#index" 根目錄這樣寫法更好看
+
+  # path 2: "/about" 關於我們，靜態網頁直接共享pages controller
+  get "/about", to: "pages#about"
 
 end
