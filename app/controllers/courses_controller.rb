e3a940132b4bc05e0a1de147d583e96469c11b52
roywangroytw <roywangroy.tw@gmail.com>
2021-11-20 17:53:19 +0800

    finished demonstrating the homework

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,8 @@
 class CoursesController < ApplicationController
 
-    def index; end   
+    def index 
+        @course_list = Course.all
+    end   
     
     def new
         @course = Course.new
@@ -19,6 +21,30 @@ class CoursesController < ApplicationController
         # redirect_to '/courses'
         # redirect_to courses_path
         # render html: params
+    end
+
+    def edit
+        @course = Course.find_by(id: params[:id])
+    end
+
+    def update
+        @course = Course.find_by(id: params[:id])
+
+        if @course.update(course_params)
+            redirect_to courses_path
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @course = Course.find_by(id: params[:id])
+
+        if @course.destroy
+            redirect_to courses_path
+        else
+            render :edit
+        end
     end
 
     private 
